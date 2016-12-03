828eaa91f73122ae35dd772209a92a02f60ac0e3
cyang019 <yangcnju@hotmail.com>
2016-12-03 16:23:23 -0500

    create assign_bbox.py

---
+++ src/assign_bbox.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def is_in_bbox(coord, bbox):
+    '''given a coordinate, determine if it's in bbox, return a boolean
+
+    parameters
+    ---------------------
+    coord: (lon,lat)
+    bbox: (llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat)
+    '''
+    lon, lat = tuple(coord)
+    llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat = tuple(bbox)
+
+    if lon < llcrnrlon or lon > urcrnrlon:
+        return False
+    if lat < llcrnrlat or lat > urcrnrlat:
+        return False
+    
+    return True
+
+
+
+
--- src/get_bbox.py
+++ src/get_bbox.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import shapefile
+import numpy as np
+import pandas as pd
 
 def get_bbox(shapes):
     '''get a list of 'bbox' attributes from shapes got from
@@ -12,6 +14,19 @@ def get_bbox(shapes):
         bboxes.append(boundaries)
     
     return bboxes
+
+def bbox_list_to_df(bboxes):
+    ''' convert a list of (llcrnrlon,llcrnrlat,urcrnrlon,urcrnrlat)'s
+    into a dataframe with each bbox associated with an ID'''
+
+    np_bboxes = np.array(bboxes)
+    columns = ['llcrnrlon','llcrnrlat','urcrnrlon','urcrnrlat']
+    df = pd.DataFrame(bboxes,columns=columns,copy=True)
+
+    return df
+
+
+    
 
 if __name__ == "__main__":
     print("function get_bbox(shapes)")
